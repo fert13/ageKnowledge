3182de375f8468d562c4e739e492830d3facc435
fert13 <nefertiti-ghislaine.dadjo@etudiant.univ-reims.fr>
2025-02-28 16:36:01 +0100

    Correction sur la configuration

--- loading.js
+++ loading.js
@@ -27,3 +27,14 @@ document.getElementById("playButton").addEventListener("click", function () {
         box2.classList.add("active")
     }
 });
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('Service Worker enregistré avec succès : ', registration);
+      }).catch(error => {
+        console.log('Enregistrement du Service Worker échoué : ', error);
+      });
+    });
+  }
