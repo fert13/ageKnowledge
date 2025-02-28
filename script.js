509d6887dbc56ae9e01f4ab38afd0450940c0762
fert13 <nefertiti-ghislaine.dadjo@etudiant.univ-reims.fr>
2025-02-28 15:40:52 +0100

    Creation d'une pwa

--- script.js
+++ script.js
@@ -1351,3 +1351,13 @@ function fire(particleRatio, opts) {
         particleCount: Math.floor(count * particleRatio)
     });
 }
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered", res))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
---
+++ serviceWorker.js
@@ -0,0 +1,41 @@
+const staticAKG = "v1"
+const assets = [
+  "/",
+  "index.html",
+  "configuration.html",
+  "game.html",
+  "introduction.html",
+  "style.css",
+  "script.js",
+  "configuration.js",
+  "introduction.js",
+  "loading.js",
+  "Assets/sfx/dice_roll.wav",
+  "Assets/sfx/pieceMove.wav",
+  "Assets/sfx/win.wav",
+  "Assets/dameOpacité.png",
+  "Assets/dé.png",
+  "Assets/Dice_1.png",
+  "Assets/Dice_2.png",
+  "Assets/Dice_3.png",
+  "Assets/Dice_4.png",
+  "Assets/Dice_5.png",
+  "Assets/Dice_6.png",
+  "Assets/image-carte2.png",
+  "Assets/image1.png",
+  "Assets/image2.png",
+  "Assets/imageblue.png",
+  "Assets/imagegreen.png",
+  "Assets/interrogatePonctuation.png",
+  "Assets/logo.png",
+  "Assets/point-d-interrogation-or.png",
+  "Assets/rollDice.gif"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticAKG).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
